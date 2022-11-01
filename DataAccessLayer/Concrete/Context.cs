cf1c4c74f8d92952ea8a613f98fa30132eedac2f
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2022-11-01 23:25:26 +0300

    ..

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -12,7 +12,7 @@ namespace DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=.;database=TraversalDb; integrated security=true;");
+            optionsBuilder.UseSqlServer("server=.; database=TraversalDb; integrated security=true;");
         }
 
         public DbSet<About> Abouts { get; set; }
