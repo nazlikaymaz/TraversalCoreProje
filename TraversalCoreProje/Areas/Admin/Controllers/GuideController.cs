e625a7478efd4fee2f49883e28266ae5d4232726
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2022-11-19 20:13:06 +0300

    comment, guide, app user, serilog

---
+++ TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,58 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _guideService.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddGuide(Guide guide)
+        {
+            _guideService.TAdd(guide);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+            var values = _guideService.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditGuide(Guide guide)
+        {
+            _guideService.TUpdate(guide);
+            return RedirectToAction("Index");
+        }
+        public IActionResult ChangetoTrue(int id)
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult ChangetoFalse(int id)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
