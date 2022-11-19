622267e361c26d8d7d40b76223d8b04e7afa56e7
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2022-11-20 00:49:14 +0300

    guide fixed and excel pdf created

--- TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,7 @@ using System.Threading.Tasks;
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("Admin/Guide")]
     public class GuideController : Controller
     {
         private readonly IGuideService _guideService;
@@ -18,41 +21,70 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
             _guideService = guideService;
         }
 
+        [Route("")]
+        [Route("Index")]
         public IActionResult Index()
         {
             var values = _guideService.TGetList();
             return View(values);
         }
+
+        [Route("AddGuide")]
         [HttpGet]
         public IActionResult AddGuide()
         {
             return View();
         }
+
+        [Route("AddGuide")]
         [HttpPost]
         public IActionResult AddGuide(Guide guide)
         {
-            _guideService.TAdd(guide);
-            return RedirectToAction("Index");
+            GuideValidator validationRules = new GuideValidator();
+            ValidationResult result = validationRules.Validate(guide);
+            if (result.IsValid)
+            {
+                _guideService.TAdd(guide);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
         }
+
+        [Route("EditGuide")]
         [HttpGet]
         public IActionResult EditGuide(int id)
         {
             var values = _guideService.TGetByID(id);
             return View(values);
         }
+
+        [Route("EditGuide")]
         [HttpPost]
         public IActionResult EditGuide(Guide guide)
         {
             _guideService.TUpdate(guide);
             return RedirectToAction("Index");
         }
+
+        [Route("ChangetoTrue/{id}")]
         public IActionResult ChangetoTrue(int id)
         {
-            return RedirectToAction("Index");
+            _guideService.TChangeToTrueByGuide(id);
+            return RedirectToAction("Index", "Guide", new { area = "Admin" });
         }
+
+        [Route("ChangetoFalse/{id}")]
         public IActionResult ChangetoFalse(int id)
         {
-            return RedirectToAction("Index");
+            _guideService.TChangeToFalseByGuide(id);
+            return RedirectToAction("Index", "Guide", new { area = "Admin" });
         }
     }
 }
---
+++ TraversalCoreProje/Controllers/PdfReportController.cs
@@ -0,0 +1,66 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StaticPdfReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya1.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+
+            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raporu");
+
+            document.Add(paragraph);
+            document.Close();
+            return File("/pdfreports/dosya1.pdf", "application/pdf", "dosya1.pdf");
+        }
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/" + "dosya2.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+            PdfPTable pdfPTable = new PdfPTable(3);
+            pdfPTable.AddCell("Misafir Adı");
+            pdfPTable.AddCell("Misafir Soyadı");
+            pdfPTable.AddCell("Misafir TC");
+
+            pdfPTable.AddCell("Ahmet");
+            pdfPTable.AddCell("Ay");
+            pdfPTable.AddCell("14524785436");
+
+            pdfPTable.AddCell("Kemal");
+            pdfPTable.AddCell("Yıldırım");
+            pdfPTable.AddCell("13524785445");
+
+            pdfPTable.AddCell("Hasan");
+            pdfPTable.AddCell("Öztürk");
+            pdfPTable.AddCell("18624785487");
+
+            document.Add(pdfPTable);
+
+            document.Close();
+            return File("/pdfreports/dosya2.pdf", "application/pdf", "dosya2.pdf");
+        }
+    }
+}
