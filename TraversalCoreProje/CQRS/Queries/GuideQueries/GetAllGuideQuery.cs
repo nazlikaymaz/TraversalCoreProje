9c0ee88e772e7c1a2680497811058aa3caf60ace
Nazlı Kaymaz <kaymaznazli4321.1@gmail.com>
2023-01-11 02:49:20 +0300

    Unit Of Work İmplementasyonu ve MediatR

---
+++ TraversalCoreProje/CQRS/Queries/GuideQueries/GetAllGuideQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProje.CQRS.Result.GuideResult;
+
+namespace TraversalCoreProje.CQRS.Queries.GuideQueries
+{
+    public class GetAllGuideQuery : IRequest<List<GetAllGuideQueryResult>>
+    {
+    }
+}
